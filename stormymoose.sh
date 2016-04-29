1a4d19e9b773cc70294649f71827e89b939379e8
Emory Merryman <emory.merryman@gmail.com>
2016-04-29 19:36:19 +0000

    correct ownership and permissions

--- stormymoose.sh
+++ stormymoose.sh
@@ -3,7 +3,9 @@
 chown jenkins:jenkins /var/lib/jenkins/credentials.xml &&
     chmod 0644 /var/lib/jenkins/credentials.xml &&
     mkdir /var/lib/jenkins/.ssh &&
+    chown jenkins:jenkins /var/lib/jenkins/.ssh &&
     chmod 0700 /var/lib/jenkins/.ssh &&
     cp /var/private/id_rsa /var/lib/jenkins/.ssh &&
-    chmod 0600 /var/lib/jenkins/.ssh &&
+    chown jenkins:jenkins /var/lib/jenkins/.ssh/id_rsa &&
+    chmod 0600 /var/lib/jenkins/.ssh/id_rsa &&
     true
