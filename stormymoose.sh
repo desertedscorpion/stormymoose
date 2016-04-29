2296093b0b67813955f79ed71ff73d776b69d89c
Emory Merryman <emory.merryman@gmail.com>
2016-04-29 20:53:20 +0000

    add dockerhost to /etc/hosts

--- stormymoose.sh
+++ stormymoose.sh
@@ -8,4 +8,5 @@ chown jenkins:jenkins /var/lib/jenkins/credentials.xml &&
     cp /var/private/id_rsa /var/lib/jenkins/.ssh &&
     chown jenkins:jenkins /var/lib/jenkins/.ssh/id_rsa &&
     chmod 0600 /var/lib/jenkins/.ssh/id_rsa &&
+    echo $(netstat -nr | grep "^0\.0\.0\.0" | awk '{print $2}') dockerhost >> /etc/hosts &&
     true
